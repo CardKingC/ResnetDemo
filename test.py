b7e6c52e86e54215323f16f7287c80ecd1164408
test <2412797074@qq.com>
2022-07-26 17:10:13 +0800

    first

---
+++ test.py
@@ -0,0 +1,68 @@
+import os
+import torch
+import torch.nn as nn
+from conf import global_settings as settings
+import torch.optim as optim
+
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+
+import time
+from datetime import datetime
+
+from models.resnet import resnet18
+
+from utils import  get_training_dataloader, get_test_dataloader, WarmUpLR, \
+    most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
+if __name__=='__main__':
+    net = resnet18()
+
+    cifar100_test_loader = get_test_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        #settings.CIFAR100_PATH,
+        num_workers=4,
+        batch_size=settings.BATCH_SIZE,
+    )
+
+    net.load_state_dict(torch.load('path_to_resnet_weights_file'))
+    print(net)
+    net.eval()
+
+    correct_1 = 0.0
+    correct_5 = 0.0
+    total = 0
+
+    with torch.no_grad():
+        for n_iter, (image, label) in enumerate(cifar100_test_loader):
+            print("iteration: {}\ttotal {} iterations".format(n_iter + 1, len(cifar100_test_loader)))
+
+            if settings.GPU:
+                image = image.cuda()
+                label = label.cuda()
+                print('GPU INFO.....')
+                print(torch.cuda.memory_summary(), end='')
+
+
+            output = net(image)
+            _, pred = output.topk(5, 1, largest=True, sorted=True)
+
+            label = label.view(label.size(0), -1).expand_as(pred)
+            correct = pred.eq(label).float()
+
+            #compute top 5
+            correct_5 += correct[:, :5].sum()
+
+            #compute top1
+            correct_1 += correct[:, :1].sum()
+
+    if settings.GPU:
+        print('GPU INFO.....')
+        print(torch.cuda.memory_summary(), end='')
+
+    print()
+    print("Top 1 err: ", 1 - correct_1 / len(cifar100_test_loader.dataset))
+    print("Top 5 err: ", 1 - correct_5 / len(cifar100_test_loader.dataset))
+    print("Parameter numbers: {}".format(sum(p.numel() for p in net.parameters())))
